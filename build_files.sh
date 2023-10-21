9344d575175ebaae9250696358d6b105eafe29cc
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2023-10-21 13:33:24 +0100

    push all file including static folder22

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,3 @@
- echo "BUILD START"
- # python3.9 -m pip install -r requirements.txt
- python3.9 -m venv venv
-    source venv/bin/activate
- pip install -r requirements.txt
- # python3.9 manage.py collectstatic
- # ls
- echo "BUILD END"
-# source venv/bin/activate
-
- # Collect static files
-echo "Collecting static files..."
-python manage.py collectstatic
+# build_files.sh
+pip install -r requirements.txt
+python3.9 manage.py collectstatic
